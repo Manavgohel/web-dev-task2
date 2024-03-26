4bb7948d0705c6c56d850e3e8c32d00eb3098e07
Manavgohel <104007875+Manavgohel@users.noreply.github.com>
2024-03-26 14:41:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let search = document.querySelector(".search-box");
+
+document.querySelector("#search-icon").onclick = () => {
+  search.classList.toggle("active");
+  menu.classList.remove("active");
+};
+
+let menu = document.querySelector(".navbar");
+
+document.querySelector("#menu-icon").onclick = () => {
+  menu.classList.toggle("active");
+  search.classList.remove("active");
+};
+
+//Hide Menu And Search Box On Scroll
+window.onscroll = () => {
+  menu.classList.remove("active");
+  search.classList.remove("active");
+};
+
+// Header
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+//for active link
+
+const navLinkEls = document.querySelectorAll(".nav__link");
+
+navLinkEls.forEach((navLinkEl) => {
+  navLinkEl.addEventListener("click", () => {
+    document.querySelector(".active")?.classList.remove("active");
+    navLinkEl.classList.add("active");
+  });
+});
